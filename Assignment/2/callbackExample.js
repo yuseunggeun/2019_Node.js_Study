205b09e73b5b3f4d62a5904e707acca1be8e8820
Yuseunggeun <yuu3730@naver.com>
2019-08-02 22:44:35 +0900

    Add assignment.2(adjust)

--- Assignment/2/callbackExample.js
+++ Assignment/2/callbackExample.js
@@ -10,14 +10,23 @@ function callbackExample(items,callback){
     },0);
 };
 */
+
 const callbackExample=(items,callback) =>{
-    setTimeout(function(){
+    setTimeout(()=>{
         const sum = items.reduce((a,b)=>(a+b));
         callback(sum);
     },0);
 };
-callbackExample(list,function(result){
-    console.log(result);
-});
+callbackExample(list,result=>{console.log(result);});
 console.log('first');
 
+/*callback function 부분을 아예 빼버린것
+const arrowExample = items =>{
+    setTimeout(() =>{
+        const sum = items.reduce((a,b)=>(a+b));
+        console.log(sum);
+    },0);
+}
+arrowExample(list);
+console.log('first');
+*/
